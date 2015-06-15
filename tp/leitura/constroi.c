4927d2d44cdf83def860bfb7c3526c4da9a591a9
Josephkzez <josephkzez@gmail.com>
2015-06-15 14:56:07 -0300

    dinamarca

--- tp/leitura/constroi.c
+++ tp/leitura/constroi.c
@@ -27,6 +27,7 @@ void ConstroiListaProjeto(ListaProjeto **epinicio, char *arquivo) {
 
    printf("Hola mundo!");
    printf("Ola world!");
+   printf("DINAMARCA");
    
    if (!pFile)
       printf("'%s' not exist\n", arquivo);
